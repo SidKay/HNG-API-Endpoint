857eb637ede3357d74cab9fcd76d0ab03b72b594
Sidney Anya <66220080+SidKay@users.noreply.github.com>
2023-09-08 11:13:14 +0100

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -11,8 +11,8 @@ info = {
     "current_day": "Thursday",
     "utc_time": "2023-08-21T15:04:05Z",
     "track": "backend",
-    "github_file_url": "https://github.com/username/repo/blob/main/file_name.ext",
-    "github_repo_url": "https://github.com/username/repo",
+    "github_file_url": "https://github.com/SidKay/HNG-API-Endpoint/blob/main/endpoint.py",
+    "github_repo_url": "https://github.com/SidKay/HNG-API-Endpoint",
     'status_code': 200
   }
 
