e409a6603462bf42998738a822cd9479cb9b795a
Sidney Anya <66220080+SidKay@users.noreply.github.com>
2023-09-08 18:12:53 +0100

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -3,19 +3,6 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-curr_day = datetime.utcnow().strftime("%A")
-curr_time = datetime.utcnow().strftime('%H:%M:%S %p UTC')
-
-info = {
-    "slack_name": "Obinna Anya",
-    "current_day": curr_day,
-    "utc_time": curr_time,
-    "track": "backend",
-    "github_file_url": "https://github.com/SidKay/HNG-API-Endpoint/blob/main/endpoint.py",
-    "github_repo_url": "https://github.com/SidKay/HNG-API-Endpoint",
-    'status_code': 200
-  }
-
 @app.route('/')
 def main() -> str:
   return 'Hello There!'
@@ -28,10 +15,15 @@ def get_info():
   response_data = {}
 
   if name and track:
-    response_data['slack_name'] = name
-    response_data['track'] = track
-  else:
-    response_data = info
+    response_data = {
+        "slack_name": "Obinna Anya",
+        "current_day": datetime.utcnow().strftime("%A"),
+        "utc_time": datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "track": "backend",
+        "github_file_url": "https://github.com/SidKay/HNG-API-Endpoint/blob/main/endpoint.py",
+        "github_repo_url": "https://github.com/SidKay/HNG-API-Endpoint",
+        'status_code': 200
+    }
 
   return jsonify(response_data)
 
